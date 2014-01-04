61d7110e647ace6a2b987a4288df6308e6b0598f
Andreas Eger <dev@eger-andreas.de>
2014-01-04 19:33:19 +0100

    update twitter api to use max_id, add older than feature

--- tpic.rb
+++ tpic.rb
@@ -14,36 +14,70 @@ Twitter.configure do |config|
   config.oauth_token_secret = auth['secret']
 end
 
-def get_tweets user, count
+# def get_tweets user, count, offset
+#   first_page =  if offset
+#                   offset/100 + 1
+#                 else
+#                   1
+#                 end
+#   page_indizies = [first_page]
+#   page_count = count/100
+#   (page_count - 1 ).times do
+#     page_indizies.push(page_indizies.last+1)
+#   end
+#   tweets = []
+#   until page_indizies.empty? do
+#     idx = page_indizies.shift
+#     p [100, idx]
+#     # tweets << Twitter.user_timeline(user, count: 200, page: idx)
+#   end
+#   remaining = count - page_count*100
+#   if remaining > 0
+#     p [remaining, first_page + page_count ]
+#     # tweets << Twitter.user_timeline(user, count: remaining, page: first_page + page_count + 1 )
+#   end
+#   p "fetched tweets from ##{first_page*100} - ##{100*(page_count+first_page)}"
+#   tweets.flatten
+# end
+
+def collect_with_max_id(collection=[], count=200, max_id=nil, &block)
+  response = nil
   if count > 200
-    tweets = []
-    pages = (count/200)
-    pages.times do |p|
-      tweets << Twitter.user_timeline(user, count: 200, page: p+1)
-    end
-    remaining = count - pages*200
-    if remaining > 0
-      tweets << Twitter.user_timeline(user, count: remaining, page: pages+2)
-    end
+    response = yield max_id, 200
+    count -= 200
   else
-    tweets = Twitter.user_timeline(user, count: count)
+    response = yield max_id, count
+    count = 0
   end
-  tweets.flatten
+  collection += response
+  response.empty? ? collection.flatten : collect_with_max_id(collection, count, response.last.id - 1, &block)
+end
+
+def get_tweets(user, count=50, max_id=nil)
+  collect_with_max_id([], count, max_id) do |max_id|
+    options = {:count => 200, :exclude_replies => true}
+    options[:max_id] = max_id unless max_id.nil?
+    Twitter.user_timeline(user, options)
+  end
 end
 
 def extract_image_url tweet
   tweet.media.map do |e|
-    if e.sizes[:large]
-      "#{e.media_url}:large"
-    else
+    # if e.sizes[:large]
+      # "#{e.media_url}:large"
+    # else
       e.media_url
-    end
+    # end
   end
 end
 def download path, url
+  path = "#{PIC_BASE}/#{path}"
   return if File.exists?(path)
-  puts "downloading #{url}"
-  open("#{PIC_BASE}/#{path}",'wb') {|f| f << open(url).read }
+  puts "downloading #{url} as #{path}"
+  open(path,'wb') {|f| f << open(url).read }
+rescue => e
+  p e
+  return
 end
 
 def download_images user, images
@@ -52,10 +86,10 @@ def download_images user, images
     next if img['url'].nil?
     img['timestamp'] = img['timestamp'].gsub(' ','0')
     if img['url'].size == 1
-      download("#{user}/#{img['timestamp']}.jpg", img['url'].first)
+      download("#{user}/#{img['timestamp']}_#{img['id']}.jpg", img['url'].first)
     else
       img['url'].each.with_index do |url,index|
-        download("#{user}/#{img['timestamp']}-#{index}.jpg", url)
+        download("#{user}/#{img['timestamp']}_#{img['id']}(#{index}).jpg", url)
       end
     end
   end
@@ -63,35 +97,76 @@ end
 def run_cached user
   download_images user, JSON.parse(IO.read("#{PIC_BASE}/#{user}/images.json"))
 end
-def run user, count: 200
-  unless File.exists?(user) && Dir.exists?(user)
-    FileUtils.mkdir "#{PIC_BASE}/#{user}"
+def run user, count: nil, max_id: nil
+  base = "#{PIC_BASE}/#{user}"
+  unless File.exists?(base) && Dir.exists?(base)
+    FileUtils.mkdir base
   end
-  tweets = get_tweets user, count
+  tweets = get_tweets user, count, max_id
   images =  tweets.map do |tweet|
     urls = extract_image_url(tweet)
     next if urls.nil? || urls.empty?
-    { 'timestamp' => tweet.created_at.strftime('%y%m%d-%H%M'),
-      'url' => urls }
+    { 'timestamp' => tweet.created_at.strftime('%Y%m%d+%H%M'),
+      'url' => urls,
+      'id' => tweet.id }
   end
-  IO.write("#{PIC_BASE}/#{user}/images.json", images.to_json)
+  IO.write("#{base}/images.json", images.to_json)
+  p images.first['id']
   download_images user, images
+  p images.last['id']
 end
 
-case ARGV.size
-when 0
-  puts 'no user given'
-  exit 1
-when 1
-  user = ARGV.first
-  puts "getting picture in the last 200 tweets for user #{user}"
-  run user
-when 2
-  user = ARGV.first
-  count = ARGV[1].to_i
-  puts "getting picture in the last #{count} tweets for user #{user}"
-  run user, count: count
-else
-  p ARGV
+require "optparse"
+
+options = {count: 200}
+user = nil
+ARGV.options do |opts|
+  opts.banner = "Usage:  #{File.basename($PROGRAM_NAME)} [OPTIONS] TWITTER_HANDLE"
+
+  opts.separator ""
+  opts.separator "Specific Options:"
+  opts.on( "-c", "--count COUNT", Integer,
+           "Count of tweets to search for images" ) do |opt|
+    options[:count] = opt
+  end
+  opts.on( "-m", "--max-id MAXID", Integer,
+           "max_id of newest tweet -> only load older tweets" ) do |opt|
+    options[:max_id] = opt
+  end
+
+  opts.separator "Common Options:"
+
+  opts.on( "-h", "--help",
+           "Show this message." ) do
+    puts opts
+    exit
+  end
+
+  begin
+    opts.parse!
+    user = ARGV.first
+  rescue
+    puts opts
+    exit
+  end
 end
+
+run user, options
+
+# case ARGV.size
+# when 0
+#   puts 'no user given'
+#   exit 1
+# when 1
+#   user = ARGV.first
+#   puts "getting picture in the last 200 tweets for user #{user}"
+#   run user
+# when 2
+#   user = ARGV.first
+#   count = ARGV[1].to_i
+#   puts "getting picture in the last #{count} tweets for user #{user}"
+#   run user, count: count
+# else
+#   p ARGV
+# end
 
